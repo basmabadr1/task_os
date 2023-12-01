6af33ddeba574eaa45a6e156c3c9b590fcda423e
basmabadr1 <basmabadrbadr272@gmail.com>
2023-12-01 23:07:58 +0200

    log

---
+++ basma/src/basma/Basma.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basma;
+
+/**
+ *
+ * @author NOUR SOFT
+ */
+public class Basma {
+
+    public static void main(String[] args) {
+        System.out.print("Basma Badr");
+    }
+    
+}
